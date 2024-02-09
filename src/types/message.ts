909951cfaa36d5f0d22d557c746d46c65aa0e6db
Pradip Chaudhary <chaudharypradip.n@gmail.com>
2024-02-09 12:34:47 +0530

    Commiting changes before changing DB ORM to drizzle

---
+++ src/types/message.ts
@@ -0,0 +1,15 @@
+import { AppRouter } from "@/trpc";
+import { inferRouterOutputs } from "@trpc/server";
+
+type RouterOutput = inferRouterOutputs<AppRouter>
+
+type Messages = RouterOutput["getFileMessages"]["messages"]
+
+type OmitText = Omit<Messages[0], "text">
+
+type ExtendedText = {
+    text: string | JSX.Element
+}
+
+// Try to make this show all properties
+export type ExtendedMessage = OmitText & ExtendedText
